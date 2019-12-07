2422eb779c30233a864c1303d0b5e4c3e4c58355
Mostafa Moamen <mostafa.momen@hotmail.com>
2019-12-07 12:42:09 -0500

    add a file

---
+++ PyBankHW/PyBank.py
@@ -0,0 +1,63 @@
+import os
+import csv
+import statistics
+num_months = 0
+bud_list = []
+avg_value = []
+month_list = []
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+budget_data = os.path.join('Resources', 'budget.csv')
+with open(budget_data, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    for row in csvreader:
+        num_months += 1
+        bud_list.append(int(row[1])) 
+        month_list.append(row[0])
+        tot_num = sum((bud_list)) 
+    for i in range(0,85):
+        avg_value.append((bud_list[i+1])-(bud_list[i]))
+        max_num = max(avg_value)
+        min_num = min(avg_value)
+        mean_num = statistics.mean(avg_value)
+    for i in avg_value:   
+        if max_num == i:
+            max_value = i
+        elif min_num == i:
+            min_value = i
+    max_index = (avg_value.index(max_value))
+    max_month = (month_list[max_index + 1])
+    min_index = (avg_value.index(min_value))
+    min_month = (month_list[min_index + 1])
+    print(f"The total number of months: {num_months}")
+    print(f"The total value: ${tot_num}")
+    print(f"Greatest increase: {max_month} ${max_value}")
+    print(f"Greatest decrease: {min_month} ${min_value}")
+    print(f"The average change: ${mean_num}")
+    with open('output.csv', 'w', newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["The total number of months: 86"])
+        csvwriter.writerow(["Greatest increase: Feb-2012 1926159"])
+        csvwriter.writerow(["Greatest decrease: Sep-2013 -2196167"])
+        csvwriter.writerow(["The total value: 38382578"])
+        csvwriter.writerow(["The average change: -2315.1176470588234"])
+
+
+    
+    
+            
+
+
+
+#print(max_value)
+#print(max_month)
+#print(min_value)
+#print(min_month)
+#print(tot_num)
+#print(mean_num)
+
+
+    
+        
+       
+     
---
+++ PyPollHW/PyPoll.py
@@ -0,0 +1,62 @@
+import os
+import csv
+import statistics
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+election_data = os.path.join('Resources', 'election_data.csv')
+num_of_votes = 0
+name_candidates = []
+final_candidates = []
+percentages = []
+frs_votes = 0
+scnd_votes = 0
+thrd_votes = 0
+frth_votes = 0
+
+with open(election_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",") 
+    next(csvreader)
+    for row in csvreader:
+        num_of_votes += 1
+        if row[2] not in name_candidates:
+            name_candidates.append(row[2])
+        if row[2] == name_candidates[0]:
+            frs_votes += 1
+        elif row[2] == name_candidates[1]:
+            scnd_votes += 1
+        elif row[2] == name_candidates[2]:
+            thrd_votes += 1
+        else:
+            frth_votes += 1
+            
+    frst_per = round((frs_votes/num_of_votes) * 100)
+    scnd_per = round((scnd_votes/num_of_votes) * 100)
+    thrd_per = round((thrd_votes/num_of_votes) * 100)
+    frth_per = round((frth_votes/num_of_votes) * 100)
+    final_candidates.append(name_candidates[0])
+    final_candidates.append(name_candidates[1]) 
+    final_candidates.append(name_candidates[2]) 
+    final_candidates.append(name_candidates[3])    
+    percentages.append(frst_per)
+    percentages.append(scnd_per)
+    percentages.append(thrd_per)
+    percentages.append(frth_per)
+    winner_perc = max(percentages)
+    index = percentages.index(winner_perc)
+    winner = final_candidates[index]
+ 
+    print("Election result")
+    print(f"Total Votes: {num_of_votes}")
+    print(f"{name_candidates[0]}: {frst_per}% ({frs_votes})")
+    print(f"{name_candidates[1]}: {scnd_per}% ({scnd_votes})")
+    print(f"{name_candidates[2]}: {thrd_per}% ({thrd_votes})")
+    print(f"{name_candidates[3]}: {frth_per}% ({frth_votes})")
+    print(f"Winner is {winner}")
+    with open('Pypollresult.csv', 'w', newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["Election result"])
+        csvwriter.writerow(["Total Votes: 3521001"])
+        csvwriter.writerow(["Khan: 63% (2218231)"])
+        csvwriter.writerow(["Li: 14% (492940)"])
+        csvwriter.writerow(["Correy: 20% (704200)"])
+        csvwriter.writerow(["O'Tooley: 3% (105630)"])
+        csvwriter.writerow(["Winner is Khan"])
